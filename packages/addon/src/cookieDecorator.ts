6c84251852fd3488ca010d6d938a97e7f90a02b7
hwookim <gusdn9754@gmail.com>
2022-02-08 01:36:58 +0900

    feat: cookieDecorator

---
+++ packages/addon/src/cookieDecorator.ts
@@ -0,0 +1,41 @@
+import { DecoratorFunction, StoryContext } from '@storybook/addons';
+
+export type Cookies = {
+  [keys in string]: string;
+};
+
+export type DecoratorParameters = {
+  cookies?: Cookies;
+};
+
+export interface DecoratorContext extends StoryContext {
+  parameters: StoryContext['parameters'] & DecoratorParameters;
+}
+
+function setCookies(cookies: Cookies) {
+  for (const [key, value] of Object.entries(cookies)) {
+    document.cookie = `${key}=${value}`;
+  }
+}
+
+function clearCookies() {
+  const cookies = document.cookie.split(';');
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i];
+    const eqPos = cookie.indexOf('=');
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+  }
+}
+
+export const cookieDecorator: DecoratorFunction = (
+  storyFn,
+  { parameters }: DecoratorContext,
+) => {
+  clearCookies();
+  if (parameters && parameters.cookies) {
+    setCookies(parameters.cookies);
+  }
+  return storyFn();
+};
