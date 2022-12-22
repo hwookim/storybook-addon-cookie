f2012a236b37a3e803ad1ce6bb4ca4699596689b
hwookim <gusdn9754@gmail.com>
2022-12-22 10:48:44 +0900

    refactor: for문 foreach로 변경

--- packages/addon/src/cookieDecorator.ts
+++ packages/addon/src/cookieDecorator.ts
@@ -25,12 +25,11 @@ function setCookies(cookies: Cookies) {
 function clearCookies() {
   const cookies = document.cookie.split(';');
 
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i];
-    const eqPos = cookie.indexOf('=');
-    const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
-    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
-  }
+  cookies.forEach((cookie) => {
+    const equalPos = cookie.indexOf('=');
+    const key = equalPos > -1 ? cookie.substring(0, equalPos) : cookie;
+    document.cookie = key + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+  });
 }
 
 export const cookieDecorator: DecoratorFunction = (
