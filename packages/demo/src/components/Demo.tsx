8e17b54f92d06dc6470e6a033329dedfdc6fb872
hwookim <gusdn9754@gmail.com>
2023-03-26 00:57:29 +0900

    feat: Demo가 전체 쿠키를 보여줄 수 있도록 변경

--- packages/demo/src/components/Demo.tsx
+++ packages/demo/src/components/Demo.tsx
@@ -1,13 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import cookie from '../utils/cookie';
+import React from 'react';
 
 export default function Demo(): JSX.Element {
-  const [value, setValue] = useState<string | undefined>('');
-
-  useEffect(() => {
-    const testCookie = cookie.get('test');
-    setValue(testCookie);
-  }, []);
-
-  return <>{value || "There's nothing!"}</>;
+  return <>{document.cookie || "There's nothing!"}</>;
 }
