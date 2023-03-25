04cf53b94c98b99c5a5b65a94062b2f83fbdcbb8
hwookim <gusdn9754@gmail.com>
2023-03-26 01:29:36 +0900

    feat: cookie 변경 후 story 변경 시 패널 내용도 같이 바뀌도록 변경

--- packages/addon/src/components/PanelContent.tsx
+++ packages/addon/src/components/PanelContent.tsx
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 import { Cookie } from '../types';
 import { ObjectControl } from '@storybook/components';
 import { clearCookies, setCookies } from '../utils';
-import { useGlobals, useParameter } from '@storybook/api';
+import { useGlobals, useParameter, useStorybookState } from '@storybook/api';
 import { PARAM_KEY } from '../constants';
 
 const PanelContent: React.FC = () => {
+  const { path } = useStorybookState();
   const cookie = useParameter<Cookie>(PARAM_KEY, {});
 
+  const [story, setStory] = useState<string>();
   const [value, setValue] = useState<Cookie>();
   const [_, updateGlobals] = useGlobals();
+
+  if (path !== story && value) {
+    setStory(path);
+    setValue(cookie);
+  }
 
   const handleChange = (newValue: Cookie) => {
     clearCookies();
