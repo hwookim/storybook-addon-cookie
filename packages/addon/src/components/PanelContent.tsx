db277a0269984c8026d26c2a14a262a28a4dd09a
hwookim <gusdn9754@gmail.com>
2023-04-01 17:44:39 +0900

    feat: Add clear cookie button

--- packages/addon/src/components/PanelContent.tsx
+++ packages/addon/src/components/PanelContent.tsx
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
-import { Cookie } from '../types';
-import { ObjectControl } from '@storybook/components';
-import { clearCookies, setCookies } from '../utils';
 import { useGlobals, useParameter, useStorybookState } from '@storybook/api';
+import { IconButton, Icons, ObjectControl } from '@storybook/components';
+
 import { PARAM_KEY } from '../constants';
+import { Cookie } from '../types';
+import { clearCookies, setCookies } from '../utils';
 
 const PanelContent: React.FC = () => {
   const { path } = useStorybookState();
-  const cookie = useParameter<Cookie>(PARAM_KEY, {});
+  const defaultCookie = useParameter<Cookie>(PARAM_KEY, {});
 
-  const [story, setStory] = useState<string>();
+  const [story, setStory] = useState<string>(path);
   const [value, setValue] = useState<Cookie>();
   const [_, updateGlobals] = useGlobals();
 
   if (path !== story && value) {
     setStory(path);
-    setValue(cookie);
+    setValue(defaultCookie);
   }
 
   const handleChange = (newValue: Cookie) => {
@@ -25,13 +26,25 @@ const PanelContent: React.FC = () => {
     updateGlobals({});
   };
 
+  const handleClear = () => {
+    clearCookies();
+    setValue({});
+    updateGlobals({});
+  };
+
   return (
-    <ObjectControl
-      name="cookie"
-      onChange={handleChange}
-      theme=""
-      value={value || cookie}
-    />
+    <div style={{ padding: '10px 20px' }}>
+      <IconButton style={{ marginBottom: '5px' }} onClick={handleClear}>
+        <Icons style={{ marginRight: '5px' }} icon="trash" />
+        Clear All Cookies
+      </IconButton>
+      <ObjectControl
+        name="cookie"
+        onChange={handleChange}
+        value={value || defaultCookie}
+        theme=""
+      />
+    </div>
   );
 };
 
