a454e86e2a195a5c0d6c9c625fe3bd5496f8c32c
hwookim <gusdn9754@gmail.com>
2023-05-31 02:52:18 +0900

    chore: Delete unnecessary addons

--- packages/demo/.storybook/main.ts
+++ packages/demo/.storybook/main.ts
@@ -1,13 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-interactions',
-    'storybook-addon-cookie',
-  ],
+  addons: ['@storybook/preset-create-react-app', 'storybook-addon-cookie'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
