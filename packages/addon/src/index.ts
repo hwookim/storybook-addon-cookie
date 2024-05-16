b17d84b5c051ab12e28eec07608bcd9fbb88667d
paul-asvb <4126256+paul-asvb@users.noreply.github.com>
2024-05-16 16:52:32 +0200

    bump up to storybook 8.0.0 (#11)

--- packages/addon/src/index.ts
+++ packages/addon/src/index.ts
@@ -1,9 +1,5 @@
 export * from './cookieDecorator';
 
-if (module && module.hot && module.hot.decline) {
-  module.hot.decline();
-}
-
 export function config(entry: unknown[] = []): unknown[] {
   return [...entry, require.resolve('./preview')];
 }
